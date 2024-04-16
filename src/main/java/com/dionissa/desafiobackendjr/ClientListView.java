be53281391fc001035ed82c86be6775ae6bcef9f
Dio <dionissarodrigo@gmail.com>
2024-04-16 16:47:19 -0300

    car view updates

--- src/main/java/com/dionissa/desafiobackendjr/ClientListView.java
+++ src/main/java/com/dionissa/desafiobackendjr/ClientListView.java
@@ -2,9 +2,11 @@ package com.dionissa.desafiobackendjr;
 
 import com.dionissa.desafiobackendjr.core.Client;
 import com.dionissa.desafiobackendjr.core.Car;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
 
 @Route("client-list")
@@ -13,9 +15,13 @@ public class ClientListView extends VerticalLayout {
     private Grid<Client> clientGrid = new Grid<>(Client.class);
     private Grid<Car> carGrid = new Grid<>(Car.class);
 
-    public ClientListView() {
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public ClientListView(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+
         // Fetch client data from the API
-        RestTemplate restTemplate = new RestTemplate();
         Client[] clients = restTemplate.getForObject("http://localhost:8080/api/clients", Client[].class);
 
         // Fetch car data from the API
@@ -23,13 +29,29 @@ public class ClientListView extends VerticalLayout {
 
         // Set up the clientGrid to display client data
         clientGrid.setItems(clients);
-        clientGrid.setColumns("id", "name", "cpf", "birthDate");
+        clientGrid.setColumns("id", "name", "cpf", "birthDate", "car");
 
         // Set up the carGrid to display car data
         carGrid.setItems(cars);
         carGrid.setColumns("id", "model", "brand");
 
-        // Add the grids to the layout
-        add(clientGrid, carGrid);
+        // Add buttons for CRUD operations
+        Button addButton = new Button("Add Client");
+        addButton.addClickListener(e -> {
+            // Logic to open a form for adding a new client
+        });
+
+        Button editButton = new Button("Edit Client");
+        editButton.addClickListener(e -> {
+            // Logic to open a form for editing a client
+        });
+
+        Button deleteButton = new Button("Delete Client");
+        deleteButton.addClickListener(e -> {
+            // Logic to delete a client
+        });
+
+        // Add the grids and buttons to the layout
+        add(addButton, editButton, deleteButton, clientGrid, carGrid);
     }
 }
